1a3d389bdac6e22b7c4e3fc3479b9353f3c5e60e
DrEvilBrain <drevilbrain@gmail.com>
2018-04-23 00:59:49 -0400

    Seamless Looping Music
    TODO: Fix little stutter between intro and main. Only happens like that in Unity.

---
+++ Assets/scripts/MusicHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+
+    public AudioClip intro;
+    public AudioClip loop;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<AudioSource>().loop = true;
+        StartCoroutine(PlayLoop());
+    }
+
+    IEnumerator PlayLoop()
+    {
+        this.GetComponent<AudioSource>().clip = intro;
+        this.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(this.GetComponent<AudioSource>().clip.length);
+        this.GetComponent<AudioSource>().clip = loop;
+        this.GetComponent<AudioSource>().Play();
+    }
+
+}
